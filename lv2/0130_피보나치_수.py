5a1722386aa6cb463fdd33a91d6d13032d9c6d0b
차지원 <chajiwon100785@gmail.com>
2022-02-05 10:53:34 +0900

    Issue #13 refactor: 피보나치 수 성능 개선

--- lv2/0130_피보나치_수.py
+++ lv2/0130_피보나치_수.py
@@ -3,3 +3,32 @@ def solution(n):
     for i in range(n):
         dp[0], dp[1] = dp[0] + dp[1], dp[0]
     return dp[0] % 1234567
+
+# fast fibo matrix
+# Fn+1 	Fn		1	1
+# Fn	Fn-1	1	0
+class Matrix:
+    def __init__(self, a = 1, b = 1, c = 1, d = 0):
+        # Fn+1, Fn, Fn, Fn-1
+        self.a, self.b, self.c, self.d = a, b, c, d
+        
+    def __mul__(self, other):
+        a = self.a * other.a + self.b * other.c
+        b = self.a * other.b + self.b * other.d
+        c = self.c * other.a + self.d * other.c
+        d = self.c * other.b + self.d * other.d
+        
+        return Matrix(a, b, c, d)
+    
+    def __pow__(self, n):
+        if n == 1:
+            return Matrix()
+
+        half = self ** (n // 2)
+        return half * half if n % 2 == 0 else half * half * Matrix()
+    
+    def __mod__(self, other):
+        return self.b % other
+        
+def solution(n):
+    return Matrix() ** n % 1234567
